63caa284273e7386e7b886fe4ef7f04b6ab00f5f
Santiago Castro <santiagocastro@192.168.1.5>
2023-06-16 19:01:42 -0300

    progress wos

---
+++ index.js
@@ -0,0 +1,22 @@
+
+//de arriba a bajo y queda fijo
+// window.addEventListener('DOMContentLoaded', function() {
+//     var title = document.getElementById('title');
+//     setTimeout(function() {
+//       title.style.opacity = '1';
+//       title.style.transform = 'translateY(0)';
+//     }, 2000); 
+//   });
+
+///rebote
+// const title = document.getElementById('title');
+// title.addEventListener('animationend', () => {
+//   title.style.animation = '';
+// });
+
+
+////rebote
+const title = document.getElementById('title');
+title.addEventListener('animationend', () => {
+  title.style.animation = '';
+});
