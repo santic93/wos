b1fd5eccdc1afe4db288e0597d8ef60e2cd6141d
Santiago Castro <santiagocastro@192.168.1.3>
2023-06-18 19:17:47 -0300

    progress

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 //de arriba a bajo y queda fijo
 // window.addEventListener('DOMContentLoaded', function() {
 //     var title = document.getElementById('title');
 //     setTimeout(function() {
 //       title.style.opacity = '1';
 //       title.style.transform = 'translateY(0)';
-//     }, 2000); 
+//     }, 2000);
 //   });
 
 ///rebote
@@ -14,9 +13,18 @@
 //   title.style.animation = '';
 // });
 
-
 ////rebote
 const title = document.getElementById('title');
 title.addEventListener('animationend', () => {
   title.style.animation = '';
 });
+
+////mostrar oculto
+function mostrarPrincipal() {
+  var principalSection = document.getElementById('principal');
+  principalSection.style.display = 'block';
+  //principalSection.classList.add("mostrar");
+}
+import data from './data.json';
+
+console.log(data);
